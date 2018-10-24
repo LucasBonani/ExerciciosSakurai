ddc97bb3ffbfb2b6c19489de9bd822eeb04af5ca
Lucas <lucas.bonani@gmail.com>
2018-10-23 22:32:06 -0300

    questão 3

--- ExerciciosSakurai/script.js
+++ ExerciciosSakurai/script.js
@@ -1,23 +1,40 @@
 /*--------------------INÍCIO DO SCRIPT QUESTÃO 1--------------------------*/
     //função para retornar Id de elementos html
-    function id(id){    
+    function getId(id){    
         return document.getElementById(id)    
     }   
-    //função para transformar em valor reservado na variável em float
+    function getId2(id){    
+        return document.getElementById(id).value   
+    }   
+    //função para transformar em valor reservado no input do html e transformar em float
     function getValor(val){    
-        var valor = id(val).value.replace(',','.')    
+        var valor = getId(val).value.replace(',','.')    
         return parseFloat(valor)    
     }    
     //função para calcular média de valor e lançar resposta no input indicado
     function clicar(){    
         var total = (getValor('minima')+getValor('maxima')) / 2    
-        id('medio').value = total    
+        getId('medio').value = total    
     }
 /*----------------------FIM DO SCRIPT QUESTÃO 1---------------------------*/
 /*--------------------INÍCIO DO SCRIPT QUESTÃO 2--------------------------*/
     //função para reseervar cotação do dólar em relação ao real
     function conversor(){
         var real = getValor('cotacaoDolar') * getValor('dolar')
-        id('convertidoReal').value = real 
+        getId('convertidoReal').value = real 
     }
 /*----------------------FIM DO SCRIPT QUESTÃO 2---------------------------*/
+/*----------------------INÍCIO DO SCRIPT QUESTÃO 3---------------------------*/
+    //função para escrever texto na tela para o usuário visualizar
+    function escreve(texto){
+        document.write(texto)
+    }  
+    //função para cálculo de comissão por peça vendida
+    function caculaComissao(){
+        var porcentagemComissao = 0.05
+        var comissao = (getValor('valorUnitario') * getValor('qtdVenda')) * porcentagemComissao
+        getId('comissao').value = comissao
+        escreve( "O " + getId2('idVendedor') + " receberá R$ " + comissao + " de comissão, por ter vendido " + getValor('qtdVenda') 
+         + " peças do código: " + getValor('codPeca'))        
+    }
+/*--------------------FIM DO SCRIPT QUESTÃO 3--------------------------*/
